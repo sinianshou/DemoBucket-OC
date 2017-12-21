d2ce6ace209c473fbea849d1455ac90dc61aedda
easer <liuyuze@9hcom.com>
2017-12-21 23:49:33 +0800

    StackAndQueue

--- DemoBucket-OC/DataStructAndAlgorithm/StackAndQueue.h
+++ DemoBucket-OC/DataStructAndAlgorithm/StackAndQueue.h
@@ -42,12 +42,47 @@ typedef struct
 
 @interface StackAndQueue : NSObject
 
+//初始化顺序栈代码
+void initStack(SqStack *st);
+//判断栈空代码
+int isEmpty(SqStack st);
+//进栈代码
+int push(SqStack *st, int x);
+//出栈代码
+int pop(SqStack *st,int *x);
+//链栈的初始化代码
+void initStack_L(LNode **lst);
+//判断栈空代码
+int isEmpty_L(LNode *lst);
+//进栈代码
+void push_L(LNode *lst, int x);
+//出栈代码
+int pop_L(LNode *lst, int *x);
+//初始化队列
+void initQueue(SqQueue *qu);
+//判断队空
+int isQueueEmpty(SqQueue qu);
+//进队算法
+int enQueue(SqQueue *qu, int x);
+//出队算法
+int deQueue(SqQueue *qu, int *x);
+//初始化链队
+void initQueue_L(LiQueue **lqu);
+//判断队空
+int isQueueempty_L(LiQueue *lqu);
+//入队算法
+void enQueue_L(LiQueue *lqu, int x);
+//出队算法
+int deQueue_L(LiQueue *lqu, int *x);
+
 @end
 /*
  栈的定义：一种只能在一端进行插入和删除操作的线性表。其中允许插入和删除操作的一端乘坐栈顶，另一端称作栈底。栈的插入和删除操作一般称为入栈和出栈。
  栈的特点：先进后出FILO。
  栈的存储结构：顺序栈和链栈。
- 站的数学性质：当n个元素以某种顺序进栈，并可在任意时刻出栈，所获得的元素排列的数目N恰好满足函数Catalan()的计算，即 N = Cn2n*1/(n+1)
+ 栈的数学性质：当n个元素以某种顺序进栈，并可在任意时刻出栈，所获得的元素排列的数目N恰好满足函数Catalan()的计算，即 N = Cn2n*1/(n+1)
+ 顺序栈的几个状态：栈空状态、栈满状态、非法状态（上溢和下溢）。
+ 顺序栈的应用：判断表达式中括号是否正确配对、求后缀式的数值
  
  队列的定义：仅允许在一端（队尾Rear）进行插入在另一端（队头Front）进行删除操作的线性表。插入和删除操作分别称为入队和出队。
  队列的特点：先进先出FIFO。
--- DemoBucket-OC/DataStructAndAlgorithm/StackAndQueue.m
+++ DemoBucket-OC/DataStructAndAlgorithm/StackAndQueue.m
@@ -12,5 +12,167 @@
 
 @implementation StackAndQueue
 
+//初始化顺序栈代码  //int stack[maxSize]; int top=-1;
+void initStack(SqStack *st)
+{
+    st->top = -1;
+}
+//判断栈空代码
+int isEmpty(SqStack st)
+{
+    if (st.top == -1) {
+        return 1;
+    }else
+    {
+        return 0;
+    }
+}
+//进栈代码  //stack[++top] = x;
+int push(SqStack *st, int x)
+{
+    if (st->top == maxSize-1) {
+        return 0;
+    }
+    ++(st->top);
+    st->data[st->top] = x;
+    return 1;
+}
+//出栈代码  //x = stack[top--];
+int pop(SqStack *st,int *x)
+{
+    if (st->top == -1) {
+        return 0;
+    }
+    x = &st->data[st->top];
+    --(st->top);
+    return 1;
+}
+
+//链栈的初始化代码
+void initStack_L(LNode **lst)
+{
+    lst = (LNode**)malloc(sizeof(LNode));
+    (**lst).next = nil;
+}
+//判断栈空代码
+int isEmpty_L(LNode *lst)
+{
+    if (lst->next == nil) {
+        return 1;
+    }else
+    {
+        return 0;
+    }
+}
+//进栈代码
+void push_L(LNode *lst, int x)
+{
+    LNode *p;
+    p = (LNode*)malloc(sizeof(LNode));
+    p->next = nil;
+    p->next = lst->next;
+    p->data = x;
+    lst->next = p;
+}
+//出栈代码
+int pop_L(LNode *lst, int *x)
+{
+    LNode *p;
+    if (lst->next == nil) {
+        return 0;
+    }
+    p = lst->next;
+    x = &p->data;
+    lst->next = p->next;
+    free(p);
+    return 1;
+}
+//初始化队列
+void initQueue(SqQueue *qu)
+{
+    qu->front = qu->rear = 0;
+}
+//判断队空
+int isQueueEmpty(SqQueue qu)
+{
+    if (qu.front == qu.rear) {
+        return 1;
+    }else
+    {
+        return 0;
+    }
+}
+//进队算法
+int enQueue(SqQueue *qu, int x)
+{
+    if ((qu->rear+1)%maxSize == qu->front) {
+        return 0;
+    }
+    qu->rear = (qu->rear+1)%maxSize;
+    qu->data[qu->rear] = x;
+    return 1;
+}
+//出对算法
+int deQueue(SqQueue *qu, int *x)
+{
+    if (qu->front == qu->rear) {
+        return 0;
+    }
+    qu->front = (qu->front+1)%maxSize;
+    *x=qu->data[qu->front];
+    return 1;
+}
+//初始化链队
+void initQueue_L(LiQueue **lqu)
+{
+    lqu = (LiQueue**)malloc(sizeof(LiQueue));
+    (**lqu).front=(**lqu).rear=nil;
+}
+//判断队空
+int isQueueempty_L(LiQueue *lqu)
+{
+    if (lqu->rear == nil || lqu->front == nil) {
+        return 1;
+    }else
+    {
+        return 0;
+    }
+}
+//入队算法
+void enQueue_L(LiQueue *lqu, int x)
+{
+    Qnode *p;
+    p = (Qnode*)malloc(sizeof(Qnode));
+    p->data = x;
+    p->next = nil;
+    if (lqu->rear == nil || lqu->front == nil) {
+        lqu->rear = lqu->front = p;
+    }else
+    {
+        lqu->rear->next = p;
+        lqu->rear = p;
+    }
+    
+}
+//出队算法
+int deQueue_L(LiQueue *lqu, int *x)
+{
+    if (lqu->rear == nil || lqu->front == nil) {
+        return 0;
+    }else
+    {
+        Qnode *p = lqu->front;
+        if (lqu->rear == lqu->front) {
+            lqu->rear = lqu->front = nil;
+        }else
+        {
+            lqu->front = lqu->front->next;
+        }
+        *x = p->data;
+        free(p);
+        return 1;
+    }
+}
+
 @end
 
