a9dd47934b0833b85c77fd0ff119f5059b4727cd
easer <liuyuze@9hcom.com>
2017-12-29 21:20:36 +0800

    CommitGeneralizedTable
    CommitGeneralizedTable

---
+++ DemoBucket-OC/DataStructAndAlgorithm/TreeAndBinaryTree.h
@@ -0,0 +1,13 @@
+//
+//  TreeAndBinaryTree.h
+//  DemoBucket-OC
+//
+//  Created by Easer on 2017/12/29.
+//  Copyright © 2017年 Liu Easer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TreeAndBinaryTree : NSObject
+
+@end
